dae5ee9b65e88bb57819a4d1d6d017e40d9919ff
e.nigma <60445182+BeBeeMan@users.noreply.github.com>
2022-04-04 19:02:21 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b, c, del;
+    while (cin >> a >> b >> c)
+    {
+        if (a != 0)
+        {
+            del = (b * b) + (4 * (a * c)) * -1;
+            if (del < 0)
+            {
+                cout << "0\n";
+            }
+            else if (del == 0)
+            {
+                cout << "1\n";
+            }
+            else
+            {
+                cout << "2\n";
+            }
+        }
+    }
+}
